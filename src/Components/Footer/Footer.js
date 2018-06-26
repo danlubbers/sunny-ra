0c6f7325823bae468940fda7def72159938da34e
Dan Lubbers <lubbers.dan@gmail.com>
2018-06-26 10:51:10 -0600

    added footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Footer() {
     return(
         <div>
-            <h1>footer</h1>
+            <footer>
+                <div className='footer-content'>
+                    <h1>ALL IMAGES COPYRIGHT @ SUNNY RA | SITE BY <a className='danlubbers' href="http://danlubbers.com">DAN LUBBERS </a></h1>
+                </div>
+            </footer>
         </div>
     )
 }
