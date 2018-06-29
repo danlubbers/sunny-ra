75446b73a2eb52f7b41393a01c32c7bd641458d9
Dan Lubbers <lubbers.dan@gmail.com>
2018-06-29 13:33:55 -0400

    adding test images for carousel

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -2,34 +2,40 @@ import React, {Component} from 'react';
 import Slider from 'react-slick';
 
 import blacksand from '../../assets/images/black-sand.jpg';
+import mammoth from '../../assets/images/mammoth.jpg';
 
 export default class Carousel extends Component {
+    
     render() {
 
-        const settings = {
+        var settings = {
             dots: true, 
             arrows: false,
             infinite: true,
             speed: 500,
             slidesToShow: 1,
-            slidesToSlide: 4,
-            autoplay: true, 
+            slidesToSlide: 2,
+            autoplay: false, 
             autoPlaySpeed: 500,
             cssEase: "linear",
             pauseOnHover: false,
             adaptiveHeight: true,
-            initialSlide: 0
+            initialSlide: 0,
+            centerMode: true,
         };
     
-    return(
+    return (
         <div className='carousel-container'>
             <Slider ref={slider => (this.slider = slider)}{...settings} style={{position: 'relative',    zIndex: '500 !important'}}>
-            </Slider>
-            <div className='carousel'>
+          
                 <div>
                     <img className="carousel-image" src={blacksand} alt="Black Sand"/>
                 </div>
-            </div>
+                <div>
+                    <img className="carousel-image" src={mammoth} alt="Mammoth"/>
+                </div>
+            
+            </Slider>
         </div>
     )
     }
