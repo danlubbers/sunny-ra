fbaed6ac13548e0a1aad57f9fe35c6831f2802c4
Dan Lubbers <lubbers.dan@gmail.com>
2018-06-30 13:51:21 -0400

    about transition works with dummy content

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-export default class About extends Component {
-    constructor(props) {
-    super(props)
+// export default class About extends Component {
+//     constructor(props) {
+//     super(props)
 
-        this.state = {
-            showAbout: false
-        };
-        this.handleClick = this.handleClick.bind(this);
-    }
+//         this.state = {
+//             showAbout: false
+//         };
+//         this.handleClick = this.handleClick.bind(this);
+//     }
 
-    handleClick() {
-        this.setState({showAbout: !this.state.showMenu})
-    }
+//     handleClick() {
+//         this.setState({showAbout: !this.state.showMenu})
+//     }
 
-    render() {
-        let {showAbout} = this.state;
-        let slideCSS = showAbout ? 'slide-about slide-about-position' : 'slide-about';
-        return(
-            <div className={slideCSS}>
-                <h1>About section</h1>
-            </div>
-        )
-    }
-}
+//     render() {
+//         let {showAbout} = this.state;
+//         let slideCSS = showAbout ? 'slide-about slide-about-position' : 'slide-about';
+//         return(
+//             <div className={slideCSS}>
+//                 <h1>About section</h1>
+//             </div>
+//         )
+//     }
+// }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom';
 import SunnyLogo from '../../assets/sunny-ra-logo.png'
 
-export default function Header() {
+export default class Header extends Component {
+    constructor(props) {
+        super(props)
+    
+            this.state = {
+                showAbout: false
+            }
+            this.handleClick = this.handleClick.bind(this);
+        }
+    
+        handleClick() {
+            this.setState({showAbout: !this.state.showAbout})
+        }
+
+    render() {
+        let {showAbout} = this.state;
+        let slideCSS = showAbout ? 'slide-about slide-about-position' : 'slide-about';
     return(
         <div>
             <header>
@@ -16,12 +32,19 @@ export default function Header() {
                         <Link to='/gallery'><li className='nav-text'>Photo Gallery</li></Link>
                         <Link to='/commissioned'><li className='nav-text'>Commissioned Work</li></Link>
                         {/* <Link to='/about'> */}
-                        <li><button className='aboutBtn'>About</button></li>
+                        <li><button className='aboutBtn' onClick={this.handleClick} >About</button></li>
                         {/* </Link> */}
                         <Link to='/contact'><li className='nav-text'>Contact</li></Link>
+
                     </ul>
                 </nav>
+                        <div className={slideCSS}>
+                            <div>
+                                <h1>ABOUT TESTING</h1>
+                            </div>
+                        </div>
             </header>
         </div>
-    )
+        )
+    }
 }
