9c79a6c82ebc1adfb24ea108a65a7df2a261922d
Dan Lubbers <lubbers.dan@gmail.com>
2018-07-03 16:14:41 -0400

    paintings 2014 and 2015 components added

---
+++ src/Components/Paintings/Paintings2014/Paintings2014.js
@@ -0,0 +1,108 @@
+import React, {Component} from 'react';
+import Slider from 'react-slick';
+
+import paintings20141 from '../../../assets/images/paintings/2014/paintings2014-1.jpg';
+import paintings20142 from '../../../assets/images/paintings/2014/paintings2014-2.jpg';
+import paintings20143 from '../../../assets/images/paintings/2014/paintings2014-3.jpg';
+import paintings20144 from '../../../assets/images/paintings/2014/paintings2014-4.jpg';
+import paintings20145 from '../../../assets/images/paintings/2014/paintings2014-5.jpg';
+import paintings20146 from '../../../assets/images/paintings/2014/paintings2014-6.jpg';
+import paintings20147 from '../../../assets/images/paintings/2014/paintings2014-7.jpg';
+import paintings20148 from '../../../assets/images/paintings/2014/paintings2014-8.jpg';
+
+
+
+export default class Carousel extends Component {
+    
+    render() {
+
+        var settings = {
+            dots: true, 
+            arrows: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToSlide: 9,
+            autoplay: true, 
+            autoPlaySpeed: 1000,
+            cssEase: "linear",
+            pauseOnHover: true,
+            adaptiveHeight: true,
+            initialSlide: 0,
+            centerMode: true,
+            centerPadding: 0,
+        };
+    
+    return (
+        <div className='carousel-container'>
+            <Slider ref={slider => (this.slider = slider)}{...settings} style={{position: 'relative',    zIndex: '500 !important'}}>
+          
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20141} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20142} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20143} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20144} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20145} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20146} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20147} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20148} alt=""/>
+                    </div>
+                </div>
+            </Slider>
+        </div>
+    )
+    }
+}
---
+++ src/Components/Paintings/Paintings2015/Paintings2015.js
@@ -0,0 +1,117 @@
+import React, {Component} from 'react';
+import Slider from 'react-slick';
+
+import paintings20151 from '../../../assets/images/paintings/2015/paintings2015-1.jpg';
+import paintings20152 from '../../../assets/images/paintings/2015/paintings2015-2.jpg';
+import paintings20153 from '../../../assets/images/paintings/2015/paintings2015-3.jpg';
+import paintings20154 from '../../../assets/images/paintings/2015/paintings2015-4.jpg';
+import paintings20155 from '../../../assets/images/paintings/2015/paintings2015-5.jpg';
+import paintings20156 from '../../../assets/images/paintings/2015/paintings2015-6.jpg';
+import paintings20157 from '../../../assets/images/paintings/2015/paintings2015-7.jpg';
+import paintings20158 from '../../../assets/images/paintings/2015/paintings2015-8.jpg';
+import paintings20159 from '../../../assets/images/paintings/2015/paintings2015-9.jpg';
+
+
+
+export default class Carousel extends Component {
+    
+    render() {
+
+        var settings = {
+            dots: true, 
+            arrows: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToSlide: 9,
+            autoplay: true, 
+            autoPlaySpeed: 1000,
+            cssEase: "linear",
+            pauseOnHover: true,
+            adaptiveHeight: true,
+            initialSlide: 0,
+            centerMode: true,
+            centerPadding: 0,
+        };
+    
+    return (
+        <div className='carousel-container'>
+            <Slider ref={slider => (this.slider = slider)}{...settings} style={{position: 'relative',    zIndex: '500 !important'}}>
+          
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20151} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20152} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20153} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20154} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20155} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20156} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20157} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20158} alt=""/>
+                    </div>
+                </div>
+                <div className='image-wrapper'>
+                    <div className='text-box'>
+                        {/* <h1></h1> */}
+                    </div>
+                    <div>
+                        <img className="carousel-image" src={paintings20159} alt=""/>
+                    </div>
+                </div>
+            </Slider>
+        </div>
+    )
+    }
+}
