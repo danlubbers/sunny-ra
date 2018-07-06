30dd18a77a950e58aa75e3ea7621cc9e1a6ea83d
Dan Lubbers <lubbers.dan@gmail.com>
2018-07-06 14:01:16 -0400

    add artist bio and artist statment

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -135,7 +135,12 @@ export default class Header extends Component {
                                 <h4>Pennsylvania Academy of the Fine Arts; Painting Certificate</h4>
                                 <h4>Penn Study Abroad; London, England</h4>
                                 <br/>
-                                <h3>Other Art Related Experience:</h3>
+                                <h3>Artist Bio:</h3>
+                                <h4>Sunny Ra most recently received a Professional Development Grant from the Great Meadows Foundation to attend the annual College Art Association Conference in New York City, the largest international conference in Visual Arts. Ra has been in many exhibitions, including Environmental Perspectives at the Pennsylvania Academy of the Fine Arts in Philadelphia. Now on Paper at the Alliance of Young Artists and Writers in New York City and had her first solo show, Nightscapes at PUBLIC Gallery in Louisville, KY in 2014. In 2012, Ra completed a three-week residency at the Vermont Studio Center under a full fellowship. Ra holds an MFA from Hunter College, CUNY in New York City and a BFA from the Univeristy of Pennsylvania and a Painting Certificate from the Pennsylvania Academy of the Fine Arts of Pennsylvania.</h4>
+                                <br/>
+                                <h3>Artist Statment:</h3>
+                                <h4>"The foundation of my work originates from my experience of growing up Korean in Louisville, Kentucky. Since I spoke little Korean and could not read or write Hangul, I was an outsider in the Korean Community. Similarly, I never quite identified myself as American since I was not white, and was living among majority white Americans. I remember people would ask me where I was from or comment on how well I spoke English. I grew up feeling and eventually believing that I did not belong anywhere, perhaps nowhere. I am ethnically Korean, but culturally, I am a combintaion of both Korean and American customs, which I am constantly negotiating and struggling with. It is from this middle ground that my night landscapes emerge and my journey into navigating the obscure begins. By layering images of the American landscape with memories, I attempt to find a space where tensions no longer exist.</h4>
+                                {/* <h3>Other Art Related Experience:</h3>
                                 <h4>Vermont Studio Center Residency</h4>
                                 <h4>Kentucky's Governor's School for the Arts</h4>
                                 <br/>
@@ -146,9 +151,9 @@ export default class Header extends Component {
                                 <h4>Vermont Studio Center Fellowship</h4>
                                 <h4>Studio Visit Magazine Springs 2012 Edition, Vol. 17 & 18</h4>
                                 <h4>Christie's Magazine Emerging Artist Presentation</h4>
-                                <h4>Gates Millennium Scholar</h4>
+                                <h4>Gates Millennium Scholar</h4> */}
                             </div>
-                            <div className='exhibition-container'>
+                            {/* <div className='exhibition-container'>
                             <h3>Exhibition History</h3>
                                 <h4>Solo Exhibition, Quills, Louisville, KY 2017</h4>
                                 <h4>Pop-Up Art Show, Republic Bank First Friday Gallery Hop, Louisville, KY  2017</h4>
@@ -171,7 +176,7 @@ export default class Header extends Component {
                                 <h4>The 104th Annual Student Exhibition, Pennsylvania Academy of Fine Arts, Philadelphia, PA – 2005</h4>
                                 <h4>The 103rd Annual Student Exhibition, Pennsylvania Academy of Fine Arts, Philadelphia, PA – 2004</h4>
                                 <h4>Certificate Student Gallery Showing,  Pennsylvania Academy of Fine Arts, Philadelphia, PA – 2001/2002</h4>
-                            </div>
+                            </div> */}
                                 <div className='xBtnAbout-container'>
                                     <button className='xBtnAbout' onClick={this.handleClickAbout}><CloseIcon/></button>
                                 </div>
