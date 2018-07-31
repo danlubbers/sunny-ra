99771b9246a67db03a99939db044f8e4b387fa66
Dan Lubbers <lubbers.dan@gmail.com>
2018-07-31 11:56:07 -0400

    trying react helmet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import routes from './routes';
 
 import 'babel-polyfill'
 
-// import {Helmet} from "react-helmet";
+import {Helmet} from "react-helmet";
 
 
 import Header from './Components/Header/Header';
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className='entire-app-wrapper'>
-        {/* <Helmet>
+        <Helmet>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />\
             <meta name="theme-color" content="#000000" />
@@ -25,7 +25,7 @@ class App extends Component {
             <title>Sunny Ra - Master Painter</title>
             <link rel="alternate" hreflang="en" href="https://sunny-ra.com" />
             <link rel="canonical" href="https://sunny-ra.com" />
-        </Helmet> */}
+        </Helmet>
         <div className="header-app">
           <Header />
         </div>
