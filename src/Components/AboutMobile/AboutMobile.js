ae25546bbb932960836094374883370277d962a0
Dan Lubbers <lubbers.dan@gmail.com>
2018-08-21 16:20:20 -0400

    forgot ul in AboutMobile

--- src/Components/AboutMobile/AboutMobile.js
+++ src/Components/AboutMobile/AboutMobile.js
@@ -16,14 +16,18 @@ export default function AboutMobile () {
                     <h3 className='artist-educator-mobile'>ARTIST / EDUCATOR</h3>
                     <br/>
                     <h3>Education:</h3>
-                    <li>Hunter College, CUNY; Master of Fine Arts</li>
-                    <li>University of Pennsylvania; Bachelor of Fine Arts, Cum Laude</li>
-                    <li>Pennsylvania Academy of the Fine Arts; Painting Certificate</li>
-                    <li>Penn Study Abroad; London, England</li>
+                        <ul>
+                            <li>Hunter College, CUNY; Master of Fine Arts</li>
+                            <li>University of Pennsylvania; Bachelor of Fine Arts, Cum Laude</li>
+                            <li>Pennsylvania Academy of the Fine Arts; Painting Certificate</li>
+                            <li>Penn Study Abroad; London, England</li>
+                        </ul>
                     <br/>
                     <h3>Other Art Related Experience:</h3>
-                    <li>Vermont Studio Center Residency</li>
-                    <li>Kentucky's Governor's School for the Arts</li>
+                        <ul>
+                            <li>Vermont Studio Center Residency</li>
+                            <li>Kentucky's Governor's School for the Arts</li>
+                        </ul>
                     <br/>
                     <h3>Artist Bio:</h3>
                     <h4>Sunny Ra most recently received a Professional Development Grant from the Great Meadows Foundation to attend the annual College Art Association Conference in New York City, the largest international conference in Visual Arts. Ra has been in many exhibitions, including Environmental Perspectives at the Pennsylvania Academy of the Fine Arts in Philadelphia. Now on Paper at the Alliance of Young Artists and Writers in New York City and had her first solo show, Nightscapes at PUBLIC Gallery in Louisville, KY in 2014. In 2012, Ra completed a three-week residency at the Vermont Studio Center under a full fellowship. Ra holds an MFA from Hunter College, CUNY in New York City and a BFA from the Univeristy of Pennsylvania and a Painting Certificate from the Pennsylvania Academy of the Fine Arts of Pennsylvania.</h4>
